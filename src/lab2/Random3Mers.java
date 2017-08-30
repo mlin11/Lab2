e2cef0c231092849c01fb7838c8d369ff235421b
mlin11 <mlin8@uncc.edu>
2017-08-30 17:23:59 -0400

    print the frequncy out

--- src/lab2/Random3Mers.java
+++ src/lab2/Random3Mers.java
@@ -6,17 +6,19 @@ public class Random3Mers
 {
 	public static void main(String[] args)
 	{
+		//Initialize count of 'AAA'
 		int count = 0;
+		//start the loop to generate and print out random 3mers
 		for(int x=0; x <1000; x++)
 		{
+			//initialize 3mer
 			String s="";
-			
+			//generate random 3mer
 			for(int y=0; y<3;y++)
 			{
 				Random random=new Random();
 				int n = random.nextInt(4);
-				
-				
+				// apply switch
 				switch(n)
 				{
 					case 0: s=s+"A";
@@ -30,12 +32,19 @@ public class Random3Mers
 				}
 				
 			}
+			//print out 3mer
 			System.out.println(s);
+			//check if it is 'AAA'; if so, increment by 1
 			if(s.equals("AAA")) 
 			{
 				count++;
 			}
 		}
-		System.out.println("The count of AAA is:" + count);
+		//print out expect count and frequency
+		double expectFreq= (double) 0.25*0.25*0.25;
+		System.out.println("The expected frequency of AAA is:" + expectFreq);
+		//print out total count and frequency
+		System.out.println("The actual frequency of AAA is:" + count/1000f);
+		
 	}
 }
