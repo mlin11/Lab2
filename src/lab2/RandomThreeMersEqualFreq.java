acc8e1993689c69c96c902f0eb4ab047c2a486ef
mlin11 <mlin8@uncc.edu>
2017-08-31 08:24:50 -0400

    final commit

---
+++ src/lab2/RandomThreeMersEqualFreq.java
@@ -0,0 +1,50 @@
+package lab2;
+
+import java.util.Random;
+
+public class RandomThreeMersEqualFreq
+{
+	public static void main(String[] args)
+	{
+		//Initialize count of 'AAA'
+		int count = 0;
+		//start the loop to generate and print out random 3mers
+		for(int x=0; x <1000; x++)
+		{
+			//initialize 3mer
+			String s="";
+			//generate random 3mer
+			for(int y=0; y<3;y++)
+			{
+				Random random=new Random();
+				int n = random.nextInt(4);
+				// apply switch
+				switch(n)
+				{
+					case 0: s=s+"A";
+					break;
+					case 1: s=s+"G";
+					break;
+					case 2: s=s+"C";
+					break;
+					case 3: s=s+"T";
+					break;
+				}
+				
+			}
+			//print out 3mer
+			System.out.println(s);
+			//check if it is 'AAA'; if so, increment by 1
+			if(s.equals("AAA")) 
+			{
+				count++;
+			}
+		}
+		//print out expect count and frequency
+		double expectFreq= (double) 0.25*0.25*0.25;
+		System.out.println("The expected frequency of AAA is:" + expectFreq);
+		//print out total count and frequency
+		System.out.println("The actual frequency of AAA is:" + count/1000f);
+		
+	}
+}
---
+++ src/lab2/RandomThreeMersUnequalFreq.java
@@ -0,0 +1,52 @@
+package lab2;
+
+import java.util.Random;
+
+public class RandomThreeMersUnequalFreq
+{
+	public static void main(String[] args)
+	{
+		//Initialize count of 'AAA'
+		int count = 0;
+		//start the loop to generate and print out random 3mers
+		for(int x=0; x <1000; x++)
+		{
+			//initialize 3mer
+			String s="";
+			//generate random 3mer
+			for(int y=0; y<3;y++)
+			{
+				Random random=new Random();
+				float f = random.nextFloat();
+				// apply if...else
+				if((f>=0) &&(f<0.12f))
+				{
+					s=s+"A";
+				}else if ((f>=0.12f) &&(f<0.5f))
+				{
+					s=s+"C";
+				}else if ((f>=0.5f) &&(f<0.89f))
+				{
+					s=s+"G";
+				}else if ((f>=0.89f) &&(f<1f))
+				{
+					s=s+"T";
+				}
+				
+			}
+			//print out 3mer
+			System.out.println(s);
+			//check if it is 'AAA'; if so, increment by 1
+			if(s.equals("AAA")) 
+			{
+				count++;
+			}
+		}
+		//print out expect count and frequency of AAA
+		double expectFreq= (double) 0.12*0.12*0.12;
+		System.out.println("The expected frequency of AAA is:" + expectFreq);
+		//print out total count and frequency
+		System.out.println("The actual frequency of AAA is:" + count/1000f);
+		
+	}
+}
